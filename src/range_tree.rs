3cc9111e43c9ec5947a6acb9af6120eec84120cb
Rufus <rufuskubedev@gmail.com>
2021-07-04 21:23:30 -0500

    Simplified self balancing tree, still not complete

--- src/range_tree.rs
+++ src/range_tree.rs
@@ -3,30 +3,24 @@ use std::fmt;
 
 pub struct Node
 {
-    i: usize,
     pub start: usize,
     pub end: usize,
     parent: Option<usize>,
-    lefty: bool, //The relationship of this node to its parent
     left: Option<usize>,
-    left_depth: usize,
     right: Option<usize>,
-    right_depth: usize,
+    depth: usize,
 }
 
 impl Node
 {
-    fn new(i: usize, parent: Option<usize>, start: usize, end: usize, lefty: bool) -> Self {
+    fn new(parent: Option<usize>, start: usize, end: usize) -> Self {
         Self {
-            i,
             start,
             end,
             parent,
-            lefty,
             left: None,
-            left_depth: 0,
             right: None,
-            right_depth: 0,
+            depth: 1,
         }
     }
 }
@@ -53,7 +47,7 @@ impl RangeTree
     pub fn new(start: usize, end: usize) -> Self {
         //Push a new node that has the start and end specified
         let mut tree_vec: Vec<Node> = Vec::new();
-        let rootnode: Node = Node::new(0,None,start,end,false);
+        let rootnode: Node = Node::new(None,start,end);
         tree_vec.push(rootnode);
         let mut intervals = HashSet::<usize>::new();
         intervals.insert(0);
@@ -67,23 +61,41 @@ impl RangeTree
 
     pub fn reinit(&mut self, start: usize, end: usize) {
         self.tree_vec.clear();
-        let rootnode: Node = Node::new(0,None,start,end,false);
+        let rootnode: Node = Node::new(None,start,end);
         self.tree_vec.push(rootnode);
         self.intervals.clear();
         self.intervals.insert(0);
     }
 
     pub fn balance(&mut self, i: usize) {
-        //Shrug the tree at a give node to balance it
-        if self.tree_vec[i].left_depth > self.tree_vec[i].right_depth {
+        let old_root: usize = i;
+        let new_root: usize;
+        let replaced_child: Option<usize>;
+        let left_depth: usize;
+        let right_depth: usize;
+        //Shrug the tree at a given node to balance it
+        match self.tree_vec[i].left {
+            Some(ld) => {left_depth = self.tree_vec[ld].depth},
+            None => {left_depth = 0}
+        }
+        match self.tree_vec[i].right {
+            Some(rd) => {right_depth = self.tree_vec[rd].depth},
+            None => {right_depth = 0}
+        }
+        if left_depth > right_depth {
             //Shrug left case
             //Obtain all relevant node indexes
-            let old_root = i;
-            let new_root = self.tree_vec[i].left.unwrap();
+            match self.tree_vec[i].left {
+                Some(nr) => {
+                    new_root = nr;
+                },
+                None => {
+                    println!("Attempting to balance favoring a node that doesn't exist!");
+                    return
+                }
+            }
             let parent = self.tree_vec[old_root].parent;
-            let orientation = self.tree_vec[old_root].lefty;
             
-            let replaced_child: Option<usize>;
             match self.tree_vec[new_root].right
             {
                 Some(rc) => {
@@ -100,21 +112,18 @@ impl RangeTree
                 Some(rc) => {
                     self.tree_vec[old_root].left = Some(rc);
                     self.tree_vec[rc].parent = Some(old_root);
-                    self.tree_vec[rc].lefty = true;
                 },
                 None => {
                     //There is no replaced child to worry about, good to go
                 }
             }
-            self.tree_vec[old_root].parent = Some(new_root);
-            self.tree_vec[old_root].lefty = false;
 
             self.tree_vec[new_root].parent = parent;
-            self.tree_vec[new_root].right = Some(self.tree_vec[old_root].i);            
+            self.tree_vec[new_root].right = Some(old_root);            
 
             match parent {
                 Some(p) => {
-                    if orientation {
+                    if self.tree_vec[new_root].end < self.tree_vec[p].start {
                         self.tree_vec[p].left = Some(new_root);
                     } else {
                         self.tree_vec[p].right = Some(new_root);
@@ -124,16 +133,25 @@ impl RangeTree
                     self.root = new_root;
                 }
             }
+
+            self.tree_vec[old_root].parent = Some(new_root);
             
-        } else if self.tree_vec[i].right_depth > self.tree_vec[i].left_depth {
-            //Shrug right case
+            //Update the depths accordingly at the operative nodes, and then all the way up
+
+        } else if left_depth < right_depth {
+            //Shrug left case
             //Obtain all relevant node indexes
-            let old_root = i;
-            let new_root = self.tree_vec[i].right.unwrap();
+            match self.tree_vec[i].right {
+                Some(nr) => {
+                    new_root = nr;
+                },
+                None => {
+                    println!("Attempting to balance favoring a node that doesn't exist!");
+                    return
+                }
+            }
             let parent = self.tree_vec[old_root].parent;
-            let orientation = self.tree_vec[old_root].lefty;
             
-            let replaced_child: Option<usize>;
             match self.tree_vec[new_root].left
             {
                 Some(rc) => {
@@ -150,28 +168,55 @@ impl RangeTree
                 Some(rc) => {
                     self.tree_vec[old_root].right = Some(rc);
                     self.tree_vec[rc].parent = Some(old_root);
-                    self.tree_vec[rc].lefty = false;
                 },
                 None => {
                     //There is no replaced child to worry about, good to go
                 }
             }
-            self.tree_vec[old_root].parent = Some(new_root);
-            self.tree_vec[old_root].lefty = true;
 
             self.tree_vec[new_root].parent = parent;
-            self.tree_vec[new_root].right = Some(self.tree_vec[old_root].i);            
+            self.tree_vec[new_root].left = Some(old_root);            
 
             match parent {
                 Some(p) => {
-                    if orientation {
-                        self.tree_vec[p].left = Some(new_root);
-                    } else {
+                    if self.tree_vec[new_root].start > self.tree_vec[p].end {
                         self.tree_vec[p].right = Some(new_root);
+                    } else {
+                        self.tree_vec[p].left = Some(new_root);
                     }
                 },
                 None => {
                     self.root = new_root;
+                }
+            }
+
+            self.tree_vec[old_root].parent = Some(new_root);
+            
+            //Now adjust depth numbers all the way up
+            let mut depth_traverser = i;
+            loop {
+                let left_depth: usize;
+                let right_depth: usize;
+                match self.tree_vec[depth_traverser].parent {
+                    Some(d) => {
+                        match self.tree_vec[d].left {
+                            Some(ld) => {left_depth = ld},
+                            None => {left_depth = 0}
+                        }
+                        match self.tree_vec[d].right {
+                            Some(rd) => {right_depth = rd},
+                            None => {right_depth = 0}
+                        }
+
+                        if left_depth > right_depth {
+                            self.tree_vec[d].depth = left_depth+1;
+                        } else {
+                            self.tree_vec[d].depth = right_depth+1;
+                        }
+                        
+                        depth_traverser = d;
+                    },
+                    None => { break }
                 }
             }
         }
@@ -200,7 +245,7 @@ impl RangeTree
                             traverser = ln;
                         },
                         None => {
-                            //We reached a split, tried to go left, but nothing is on the left
+                            println!("We reached a split, tried to go left, but nothing is on the left");
                             //This is a repeat packet on a completed interval and can be quietly ignored
                             break;
                         }
@@ -211,7 +256,7 @@ impl RangeTree
                             traverser = rn;
                         },
                         None => {
-                            //We reached a split, tried to go right, but nothing is on the right
+                            println!("We reached a split, tried to go right, but nothing is on the right");
                             //This is a repeat packet on a completed interval and can be quietly ignored
                             break;
                         }
@@ -238,44 +283,75 @@ impl RangeTree
                     self.tree_vec[traverser].end = index;
                     
                     //Push a left and right node for the split
-                    self.tree_vec[traverser].left_depth = 1;
+                    self.tree_vec[traverser].depth += 1;
+
                     self.tree_vec[traverser].left = Some(self.tree_vec.len());
                     self.intervals.insert(self.tree_vec.len());
-                    self.tree_vec.push(Node::new(self.tree_vec.len(),Some(traverser),lowside,index-1,true));
+                    self.tree_vec.push(Node::new(Some(traverser),lowside,index-1));
                     
-                    self.tree_vec[traverser].right_depth = 1;
                     self.tree_vec[traverser].right = Some(self.tree_vec.len());
                     self.intervals.insert(self.tree_vec.len());
-                    self.tree_vec.push(Node::new(self.tree_vec.len(),Some(traverser),index+1,highside,false));
+                    self.tree_vec.push(Node::new(Some(traverser),index+1,highside));
 
                     //Now adjust depth numbers all the way up
                     let mut depth_traverser = traverser;
                     loop {
+                        let left_depth: usize;
+                        let right_depth: usize;
+                        match self.tree_vec[depth_traverser].left {
+                            Some(ld) => {left_depth = self.tree_vec[ld].depth},
+                            None => {left_depth = 0}
+                        }
+                        match self.tree_vec[depth_traverser].right {
+                            Some(rd) => {right_depth = self.tree_vec[rd].depth},
+                            None => {right_depth = 0}
+                        }
+
+                        if left_depth > right_depth {
+                            self.tree_vec[depth_traverser].depth = left_depth+1;
+                        } else {
+                            self.tree_vec[depth_traverser].depth = right_depth+1;
+                        }
+
                         match self.tree_vec[depth_traverser].parent {
                             Some(d) => {
-                                if self.tree_vec[depth_traverser].lefty {
-                                    //We are a left child of our parent
-                                    //Update the parent's left depth, and then move to the parent
-                                    self.tree_vec[d].left_depth+=1;
-                                    if self.tree_vec[d].left_depth > self.tree_vec[d].right_depth+1 {
-                                        println!("We have a possible rebalance left {:?} right {:?}",self.tree_vec[d].left_depth,self.tree_vec[d].right_depth);
-                                    }
-                                } else {
-                                    //We are a right child of our parent
-                                    //Update the parent's right depth, and then move to the parent
-                                    self.tree_vec[d].right_depth+=1;
-                                    if self.tree_vec[d].right_depth > self.tree_vec[d].left_depth+1 {
-                                        println!("We have a possible rebalance left {:?} right {:?}",self.tree_vec[d].left_depth,self.tree_vec[d].right_depth);
-                                    }
-                                }
                                 depth_traverser = d;
                             },
                             None => { break }
                         }
                     }
 
-                    //Now rebalance the tree!  At any node where depths mismatch by 2 or more
-                    
+                    //Now check for rebalance opportunities all the way up
+                    let mut depth_traverser = traverser;
+                    loop {
+                        let left_depth: usize;
+                        let right_depth: usize;
+
+                        match self.tree_vec[depth_traverser].left {
+                            Some(ld) => {left_depth = self.tree_vec[ld].depth},
+                            None => {left_depth = 0}
+                        }
+                        match self.tree_vec[depth_traverser].right {
+                            Some(rd) => {right_depth = self.tree_vec[rd].depth},
+                            None => {right_depth = 0}
+                        }
+
+                        if left_depth > right_depth+1 || right_depth > left_depth+1 {
+                            println!("We have a chance to rebalance - Left: {:?} Right: {:?}",left_depth,right_depth);
+                            self.balance(depth_traverser);
+                        } else {
+                            //println!("Nah, no need to rebalance - Left: {:?} Right: {:?}",left_depth,right_depth);
+                        }
+
+                        match self.tree_vec[depth_traverser].parent {
+                            Some(d) => {
+                                depth_traverser = d;
+                            },
+                            None => { break }
+                        }
+                    }
+
+
                     break;
                 } else {
                     //We are at an interval, but the packet does not fall within the interval
